a30ad9c4271c9dcd861c7ff03f7f728f571b0e10
NamPhan <sockyone16899@gmail.com>
2020-09-24 21:24:02 +0700

    prefixcodetree commented

--- prefixcodetree.py
+++ prefixcodetree.py
@@ -53,18 +53,18 @@ class PrefixCodeTree:
         
         return decoded
 
-codeTree = PrefixCodeTree()
+# codeTree = PrefixCodeTree()
 
-codebook = {
-    'x1': [0],
-    'x2': [1,0,0],
-    'x3': [1,0,1],
-    'x4': [1,1]
-}
+# codebook = {
+#     'x1': [0],
+#     'x2': [1,0,0],
+#     'x3': [1,0,1],
+#     'x4': [1,1]
+# }
 
-for symbol in codebook:
-    codeTree.insert(codebook[symbol], symbol)
+# for symbol in codebook:
+#     codeTree.insert(codebook[symbol], symbol)
 
 
-print(codeTree.decode(b'\xd2\x9f\x20', 21))
+# print(codeTree.decode(b'\xd2\x9f\x20', 21))
 
